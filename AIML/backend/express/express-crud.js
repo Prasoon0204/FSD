75c12bc2b2713226f7290fbb348e7f60a25f3e6a
Prasoon Thakur <prasoon.thakur0204@gmail.com>
2025-03-05 12:11:21 +0530

    patch and delete

--- AIML/backend/express/express-crud.js
+++ AIML/backend/express/express-crud.js
@@ -1,13 +1,13 @@
 import express from "express";
+import fs from "fs/promises";
 
-const users = [{
-    id:1,
-    name:'Dev',
-    email:'dev@abes.ac.in'
-}]
+const data = await fs.readFile("users.json");
+const users = JSON.parse(data);
 
 const app = express();
 const port = 3000;
+
+app.use(express.json())
 
 app.get("/api",(req,res)=>{
     res.send("Welcome to my API");
@@ -19,11 +19,24 @@ app.get("/getusers/:id",(req,res)=>{
     res.send(user);
 })
 
+app.patch("/api/updateUser/:id",(req,res)=>{
+    const { id } = req.params;
+    const { name } = req.body;
+    const index = users.findIndex(user => user.id == id)
+    users[index].name = name;
+    res.send("user updated successfully");
+})
+
+app.delete("/api/deleteUser/:id",(req,res)=>{
+    const { id } = req.params;
+    const index = users.findIndex(user => user.id == id)
+    users.splice(index,1);
+    res.send("user deleted successfully");
+})
+
 app.get("/getusers",(req,res)=>{
     res.send(users);
 })
-
-app.use(express.json())
 
 app.post("/api/users", (req, res) => {
     const bodydata = req.body;
